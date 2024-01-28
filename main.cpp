b41e8b9709960d0c9edb3e8e0c76303d53aaec43
Cubitect <cubitect@gmail.com>
2024-01-28 23:28:24 +0100

    build test 8

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 
 #include <QApplication>
 
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    
+    if (argc > 1)
+    {
+        printf("Hello World!\n");
+        fflush(stdout);
+        return 0;
+    }
+    
     return a.exec();
 }
