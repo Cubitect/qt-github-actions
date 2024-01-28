9fb0b2780573087a23345903a19eb388e4c217b0
Cubitect <cubitect@gmail.com>
2024-01-29 00:38:40 +0100

    build test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <stdlib.h>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    strtol("123", 0, 10);
 }
 
 MainWindow::~MainWindow()
